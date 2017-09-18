7a51e6fb0c6a73539c744ae5b3b3c6bade72e333
Doreen Vanunu <doreen.vanunu@gmail.com>
2017-09-17 23:08:44 -0500

    Adding property files

---
+++ LogAggregationAgent/src/main/java/log/forwarding/agent/config/AppConfig.java
@@ -0,0 +1,10 @@
+package log.forwarding.agent.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "log.forwarding.agent")
+public class AppConfig {
+
+}
